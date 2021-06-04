10c6eea6079a112a7d50460833d6782ed92115fd
mmmG <a331998513@gmail.com>
2021-06-04 17:02:42 +0200

    upload images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import GlobalStyle  from './GlobalStyle'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Esssssssdit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <GlobalStyle />
+      <Header text={'shitttsssssssssss'}/>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header({text}) {
+    return (
+        <div>
+            <h2>YESSSSS {text}</h2>
+        </div>
+    )
+}
+
+export default Header
