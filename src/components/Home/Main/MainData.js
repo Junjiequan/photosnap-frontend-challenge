80f159541f0d759a55c3e55b9369eb6525d8f9e8
mmmG <a331998513@gmail.com>
2021-06-24 19:59:31 +0200

    text-space minor things fixed

--- src/components/Home/Main/MainData.js
+++ src/components/Home/Main/MainData.js
@@ -6,7 +6,7 @@ export const MainObjOne = {
     reversed:true,
     title:`Beautiful stories \nevery time.`,
     info:`We povide design templates to ensure your stories look terrific. Easily add photos, 
-            text, embed maps and media from other networks. Then share your story with everyone.`,
+            text, embed maps and media from other networks. Then share your story with everyone`,
     svgColor:'#000',
     btnText:'view the stories',
     img:mainImg1,
@@ -17,7 +17,7 @@ export const MainObjTwo = {
     reversed:false,
     title:'Designed for everyone.',
     info:`Photosnap can help you create stories that resonate with your audience. 
-            Our tool is designed for photographers of all levels, brands, businesses you name it. `,
+            Our tool is designed for photographers of all levels, brands, businesses you name it `,
     svgColor:'#000',
     btnText:'view the stories',
     img:mainImg2,
